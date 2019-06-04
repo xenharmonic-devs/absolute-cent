9b7854ef3185e3cc6a2f5b05bc47a65c916cd8e0
Lajos Meszaros <m_lajos@hotmail.com>
2019-06-04 11:24:55 +0200

    docs(project): Added crude type signature to the functions

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const semitoneRatio = nthRoot(12, 2)
 const centRatio = nthRoot(1200, 2)
 const noteIndexOfA4 = 69
 
+// calculateFrequencyOfZeroCents :: int -> float
 const calculateFrequencyOfZeroCents = memoizeWith(toString, frequencyOfA4 => {
   return frequencyOfA4 / semitoneRatio ** noteIndexOfA4
 })
 
-const toHertz = memoizeWith(toString, (absoluteCent, frequencyOfA4 = 440, precision = 10) => {
+// toHertz :: float -> int -> int -> float
+const toHertz = memoizeWith(toString, (absoluteCent, frequencyOfA4 = 440, precision = 7) => {
   return toNDecimals(precision, calculateFrequencyOfZeroCents(frequencyOfA4) * centRatio ** absoluteCent)
 })
 
-const fromHertz = memoizeWith(toString, (frequency, frequencyOfA4 = 440, precision = 10) => {
+// fromHertz :: float -> int -> int -> float
+const fromHertz = memoizeWith(toString, (frequency, frequencyOfA4 = 440, precision = 7) => {
   return toNDecimals(precision, getBaseLog(centRatio, frequency / calculateFrequencyOfZeroCents(frequencyOfA4)))
 })
 
