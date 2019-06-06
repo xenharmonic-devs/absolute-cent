a25b16dd6b979e6c83defd65a81fee44fa8a1bde
Lajos Meszaros <m_lajos@hotmail.com>
2019-06-06 18:20:23 +0200

    refactor(memoize): Removed memoize

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,7 @@
-import { memoizeWith, toString, match, tail, toLower, is, test } from 'ramda'
+import { match, tail, toLower, is, test } from 'ramda'
 import { semitonesByModifier, semitonesByKey, noteIndexOfA4, semitoneRatio } from './constants'
 
-const scientificNotationToSemitones = memoizeWith(toString, note => {
+const scientificNotationToSemitones = note => {
   const pattern = /^([a-g])(x|#|bb|b|)(-1|\d)$/i
 
   if (!is(String, note) || !test(pattern, note)) {
@@ -11,11 +11,11 @@ const scientificNotationToSemitones = memoizeWith(toString, note => {
   const result = match(pattern, note)
   const [key, modifier, octave] = tail(result)
   return (parseInt(octave) + 1) * 12 + semitonesByKey[toLower(key)] + semitonesByModifier[modifier]
-})
+}
 
 // calculateFrequencyOfZeroCents :: int -> float
-const calculateFrequencyOfZeroCents = memoizeWith(toString, frequencyOfA4 => {
+const calculateFrequencyOfZeroCents = frequencyOfA4 => {
   return frequencyOfA4 / semitoneRatio ** noteIndexOfA4
-})
+}
 
 export { scientificNotationToSemitones, calculateFrequencyOfZeroCents }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
-import { memoizeWith, toString } from 'ramda'
 import { toNDecimals, getBaseLog } from './math'
 import { scientificNotationToSemitones, calculateFrequencyOfZeroCents } from './helpers'
 import { semitoneRatio, centRatio } from './constants'
 
 // toHertz :: float -> int -> int -> float
-const toHertz = memoizeWith(toString, (absoluteCent, frequencyOfA4 = 440, precision = 7) => {
+const toHertz = (absoluteCent, frequencyOfA4 = 440, precision = 7) => {
   const result = calculateFrequencyOfZeroCents(frequencyOfA4) * centRatio ** absoluteCent
   return toNDecimals(precision, result)
-})
+}
 
 // fromHertz :: float -> int -> int -> float
-const fromHertz = memoizeWith(toString, (frequency, frequencyOfA4 = 440, precision = 7) => {
+const fromHertz = (frequency, frequencyOfA4 = 440, precision = 7) => {
   const result = getBaseLog(centRatio, frequency / calculateFrequencyOfZeroCents(frequencyOfA4))
   return toNDecimals(precision, result)
-})
+}
 
-const fromSemitones = memoizeWith(toString, (semitones, frequencyOfA4 = 440, precision = 7) => {
+const fromSemitones = (semitones, frequencyOfA4 = 440, precision = 7) => {
   const hertz = calculateFrequencyOfZeroCents(frequencyOfA4) * semitoneRatio ** semitones
   return fromHertz(hertz, frequencyOfA4, precision)
-})
+}
 
 // fromScientificNotation :: string -> int -> int -> float
-const fromScientificNotation = memoizeWith(toString, (note, frequencyOfA4 = 440, precision = 7) => {
+const fromScientificNotation = (note, frequencyOfA4 = 440, precision = 7) => {
   return fromSemitones(scientificNotationToSemitones(note), frequencyOfA4, precision)
-})
+}
 
 export { toHertz, fromHertz, fromSemitones, fromScientificNotation }
