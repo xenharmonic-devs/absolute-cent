4553c0305cad9f9698a38afe59aaedf76bb634b9
Lajos Meszaros <m_lajos@hotmail.com>
2019-06-06 17:37:37 +0200

    test(math): Added tests for every export

--- test/math.spec.js
+++ test/math.spec.js
@@ -5,10 +5,18 @@ import { nthRoot, toNDecimals, getBaseLog } from '../src/math'
 
 describe('nthRoot', () => {
   it('calculates the nth root of a number', () => {
-    assert.strictEqual(Math.round(nthRoot(3, 64)), 4)
+    assert.strictEqual(Math.round(nthRoot(3, 64) * 10000) / 10000, 4)
   })
 })
 
-describe('toNDecimals', () => {})
+describe('toNDecimals', () => {
+  it('rounds a number for a given number of decimals', () => {
+    assert.strictEqual(toNDecimals(5, 3.987654321), 3.98765)
+  })
+})
 
-describe('getBaseLog', () => {})
+describe('getBaseLog', () => {
+  it('applies the n-base logarithm to a number ', () => {
+    assert.strictEqual(getBaseLog(3, 243), 5)
+  })
+})
