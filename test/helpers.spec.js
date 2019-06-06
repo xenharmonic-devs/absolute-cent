29a3b1ba2c696e64a4c756bb950ec10b26734a79
Lajos Meszaros <m_lajos@hotmail.com>
2019-06-06 17:50:46 +0200

    test(helpers): Added tests for every export

--- test/helpers.spec.js
+++ test/helpers.spec.js
@@ -1,11 +1,22 @@
 /* global describe, it */
 
 import assert from 'assert'
-import {
-  scientificNotationToSemitones,
-  calculateFrequencyOfZeroCents
-} from '../src/helpers'
+import { scientificNotationToSemitones, calculateFrequencyOfZeroCents } from '../src/helpers'
 
-describe('scientificNotationToSemitones', () => {})
+describe('scientificNotationToSemitones', () => {
+  it('parses notes to semitones', () => {
+    assert.strictEqual(scientificNotationToSemitones('C4'), 60)
+    assert.strictEqual(scientificNotationToSemitones('A4'), 69)
+    assert.strictEqual(scientificNotationToSemitones('C#1'), 25)
+  })
+  it('starts octaves from -1', () => {
+    assert.strictEqual(scientificNotationToSemitones('C-1'), 0)
+  })
+})
 
-describe('calculateFrequencyOfZeroCents', () => {})
+describe('calculateFrequencyOfZeroCents', () => {
+  it('calculates the hertz of C-1 based on the frequency of A4', () => {
+    assert.strictEqual(Math.round(calculateFrequencyOfZeroCents(440) * 10000) / 10000, 8.1758)
+    assert.strictEqual(Math.round(calculateFrequencyOfZeroCents(432) * 10000) / 10000, 8.0271)
+  })
+})
