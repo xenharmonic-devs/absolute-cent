400a2dcdb5bae552651ff586c0f198821cd3c144
Lajos Meszaros <m_lajos@hotmail.com>
2019-06-06 17:10:01 +0200

    test(constants): Added tests for every export

--- test/constants.spec.js
+++ test/constants.spec.js
@@ -1,20 +1,41 @@
 /* global describe, it */
 
 import assert from 'assert'
-import {
-  semitoneRatio,
-  centRatio,
-  noteIndexOfA4,
-  semitonesByKey,
-  semitonesByModifier
-} from '../src/constants'
+import { is, keys } from 'ramda'
+import { semitoneRatio, centRatio, noteIndexOfA4, semitonesByKey, semitonesByModifier } from '../src/constants'
 
-describe('semitoneRatio', () => {})
+describe('semitoneRatio', () => {
+  it('raising it to the 12th power should give 2', () => {
+    assert.strictEqual(semitoneRatio ** 12, 2)
+  })
+})
 
-describe('centRatio', () => {})
+describe('centRatio', () => {
+  it('rising it to the 1200th power should give 2', () => {
+    assert.strictEqual(Math.round(centRatio ** 1200 * 10000) / 10000, 2)
+  })
+})
 
-describe('noteIndexOfA4', () => {})
+describe('noteIndexOfA4', () => {
+  it('should be 69', () => {
+    assert.strictEqual(noteIndexOfA4, 69)
+  })
+})
 
-describe('semitonesByKey', () => {})
+describe('semitonesByKey', () => {
+  it('is an object', () => {
+    assert.strictEqual(is(Object, semitonesByKey), true)
+  })
+  it('contains keys from a to g', () => {
+    assert.deepStrictEqual(keys(semitonesByKey), ['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+  })
+})
 
-describe('semitonesByModifier', () => {})
+describe('semitonesByModifier', () => {
+  it('is an object', () => {
+    assert.strictEqual(is(Object, semitonesByModifier), true)
+  })
+  it('contains keys with modifiers', () => {
+    assert.deepStrictEqual(keys(semitonesByModifier), ['x', '#', '', 'b', 'bb'])
+  })
+})
--- test/math.spec.js
+++ test/math.spec.js
@@ -1,15 +1,11 @@
 /* global describe, it */
 
 import assert from 'assert'
-import {
-  nthRoot,
-  toNDecimals,
-  getBaseLog
-} from '../src/math'
+import { nthRoot, toNDecimals, getBaseLog } from '../src/math'
 
 describe('nthRoot', () => {
   it('calculates the nth root of a number', () => {
-    assert.equal(Math.round(nthRoot(3, 64)), 4)
+    assert.strictEqual(Math.round(nthRoot(3, 64)), 4)
   })
 })
 
